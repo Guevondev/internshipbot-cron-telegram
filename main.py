bf3405b4d0eb742b520a3fe69359f9fb8ccb7a71
Kappappita <ignacioladal@gmail.com>
2023-01-09 21:27:51 -0300

    feat: message request changed to 6 days prior to date of program execution

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import datetime
 import os
 
 # Loads basic data
-offersFile = open('offers.txt', 'w', encoding='utf-8')
-load_dotenv('.env')
+load_dotenv('C:/Users/Kappappita/Desktop/telegram-internship-scrapper/.env')
 API_ID = os.getenv('API_ID')
 API_HASH = os.getenv('API_HASH')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -13,6 +12,7 @@ TEST_CHANNEL = os.getenv('TEST_CHANNEL')
 
 # Starts telegram client
 with TelegramClient('anon', API_ID, API_HASH).start() as client:
+    
     # Retrieves all available dialogs for user
     channels = {dialog.entity.title: dialog.entity \
          for dialog in client.get_dialogs() if dialog.is_channel}
@@ -22,17 +22,19 @@ with TelegramClient('anon', API_ID, API_HASH).start() as client:
     # it does, ignores the offer, otherwise, creates a dictionary
     # with author, message and source.
 
+    totalOffers = 0
+    date = datetime.date.today() - datetime.timedelta(days=6)
+    print(f'[Telegram] Retrieving messages from {date} until now')
     for message in client.iter_messages(
             channel,
-            offset_date=datetime.date(2022, 10, 25),
+            offset_date=date,
             reverse=True):
 
         if hasattr(message.media, 'photo') == False:
             message = message.message.split('\n', 1)
             offer = {"author": message[0], "offer": message[1], "source": "telegram"}
-            offersFile.write(str(offer) + ',\n')
+            # Actual HTTP request not used until deployment
+            totalOffers += 1
+
+    print(f'[Telegram] {totalOffers} messages retrieved')
             
-    offersFile.close()
-
-    
-
